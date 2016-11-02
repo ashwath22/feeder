e8531f91e90680c62be6ac3155c703f4cc542b1e
ashwath22 <ashwath.anand.22@gmail.com>
2016-11-02 14:18:46 -0700

    minor refactoring

--- js/feedr.js
+++ js/feedr.js
@@ -1,49 +1,67 @@
 // Create the Elements.
 var search = document.querySelector(".search");
 var displayDiggArticles = document.querySelector(".displayDiggArticles");
-var displayArticles2 = document.querySelector(".displayArticles2");
-var displayArticles3 = document.querySelector(".displayArticles3");
+var displayGuardianArticles = document.querySelector(".displayGuardianArticles");
+var displayBbcArticles = document.querySelector(".displayBbcArticles");
 var article = document.querySelector(".article");
 var popUp = document.querySelector(".popUp");
 
+// setup URLs for API calls.
+var diggUrl = "https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json";
+var guardianUrl = "http://content.guardianapis.com/search?q=technology&api-key=4aec2a05-eda5-4352-a802-522cfa655803";
+var bbcUrl = "https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=158fb7af227c4843879cbe7f7f2bb5b3";
+           
 var articleTemplate = document.querySelector("#article-template");
 
 // Add Event Listeners. 
-window.addEventListener('load', showArticles)
+window.addEventListener('load', showArticles);
+displayDiggArticles.addEventListener('click', showPopup);
+var diggJson = null;
+var diggId = null;
+
+// Popup
+function showPopup(event) {
+    event.preventDefault();
+    target = event.target.closest("ARTICLE");
+    id = target.id;
+    jQuery.getJSON(diggUrl, getData);
+    
+    console.log('Digg json: ', diggJson);
+} 
+
+function getData(json) {
+    console.log('json: ', json);
+    diggJson = json;
+}
 
 // Add Event Handler functions.
 function showArticles (event) {
     event.preventDefault();
-    // displayArticles.innerHTML = '';
-    var url1 = "https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json";
-    jQuery.getJSON(url1, appendArticles1);
-
-    var url2 = "http://content.guardianapis.com/search?q=technology&api-key=4aec2a05-eda5-4352-a802-522cfa655803";
-    jQuery.getJSON(url2, appendArticles2);
-
-    var url3 = "https://newsapi.org/v1/articles?source=bbc-sport&sortBy=top&apiKey=158fb7af227c4843879cbe7f7f2bb5b3";
-    jQuery.getJSON(url3, appendArticles3);
+    // JSONify!!
+    jQuery.getJSON(diggUrl, appendDiggArticles);
+    jQuery.getJSON(guardianUrl, appendGuardianArticles);
+    jQuery.getJSON(bbcUrl, appendBbcArticles);
 }
 
 // update the articles on the page.
-function appendArticles1(json) {
+function appendDiggArticles(json) {
     var articles = json.data;
-    console.log("DIGG: ", json.data);
+    // console.log("DIGG: ", json.data);
     articles.newsSource = "DIGG";
     var templateFn = Handlebars.compile(DiggArticleList.innerHTML);
     displayDiggArticles.innerHTML = templateFn(articles);
 }
 
-function appendArticles2(json) {
+function appendGuardianArticles(json) {
     json.newsSource = "The Guardian";
-    console.log("Guardian: ", json);
-    var templateFn = Handlebars.compile(articleList2.innerHTML);
-    displayArticles2.innerHTML = templateFn(json);
+    // console.log("Guardian: ", json);
+    var templateFn = Handlebars.compile(GuardianArticleList.innerHTML);
+    displayGuardianArticles.innerHTML = templateFn(json);
 }
 
-function appendArticles3(json) {
+function appendBbcArticles(json) {
     json.newsSource = "BBC sport";
-     console.log("BBC sport: ", json);
-    var templateFn = Handlebars.compile(articleList3.innerHTML);
-    displayArticles3.innerHTML = templateFn(json);   
+     // console.log("BBC sport: ", json);
+    var templateFn = Handlebars.compile(BbcArticleList.innerHTML);
+    displayBbcArticles.innerHTML = templateFn(json);   
 }
